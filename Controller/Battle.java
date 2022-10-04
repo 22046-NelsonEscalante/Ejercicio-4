d9d75600df7d7cd8849451017e0d6e6b9b558913
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2022-10-04 15:12:16 -0600

    Added attack and abilty methods. Added a dice roll method.

--- Controller/Battle.java
+++ Controller/Battle.java
@@ -2,36 +2,39 @@ package Controller;
 
 import Model.*;
 import Model.Character;
-
-import java.util.ArrayList;
+import java.util.Random;
 
 public class Battle {
-    private Character player;
-    private ArrayList<Enemy> NPC;
     private int turnCounter = 1;
 
 
-    public Battle(Character player, ArrayList<Enemy> NPC) {
-        this.player = player;
-        this.NPC = NPC;
-    }
-
-
-    public void PlayerAction() {
-        setTurnCounter(0);
-    }
-
-    public void EnemyAction() {
-        for (Enemy e: NPC) {
-
-        }
-
-        setTurnCounter(1);
-    }
-
-
-    public void Attack(Character attacker, Character target) {
+    public String EnemyAttack(Enemy attacker, Character target) {
         target.setCurrentHP((target.getCurrentHP())-(attacker.getATK()));
+        return attacker.getName() + " te ataca por " + attacker.getATK() + " de daño.";
+    }
+
+    public String PlayerAttack(Character attacker, Enemy target) {
+        target.setCurrentHP((target.getCurrentHP())-(attacker.getATK()));
+        return "Atacas por " + attacker.getATK() + " de daño.";
+    }
+
+    public String Ability(Enemy attacker, Character target) {
+        int roll = d20();
+        switch (attacker.getAbility()) {
+            case "Fotosíntesis":
+                double healCalc = attacker.getCurrentHP()*0.5*(roll/20);
+                int heal = (int)healCalc;
+                attacker.setCurrentHP(attacker.getCurrentHP() + heal);
+                return "La " + attacker.getName() + " absorbe luz solar y se cura " + heal + "PS!";
+            case "Afilar":
+                double ATKcalc = attacker.getATK() * 1.2;
+                int ATK = (int)ATKcalc;
+                attacker.setATK(ATK);
+                return "El " + attacker.getName() + " afila sus garras!";
+            default:
+                /*Texto que solo se lee en caso de no definir una habilidad, como un placeholder. */
+                return "El enemigo gruñe...";
+        }
     }
 
 
@@ -41,5 +44,13 @@ public class Battle {
 
     public int getTurnCounter() {
         return turnCounter;
+    }
+
+    public int d20() {
+        Random rand = new Random();
+
+        int d20 = rand.nextInt(20);
+        d20 += 1;
+        return d20;
     }
 }
